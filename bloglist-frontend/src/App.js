b598ab16e9a2209aa30e8e4e1d4e5d05a1014432
antjuntu <antjuntu@gmail.com>
2019-06-11 17:25:56 +0300

    5.8*: blogilistan frontend, step8

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -117,7 +117,7 @@ const App = () => {
       </Togglable>
       <br />
 
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} handleLikesUpdate={handleLikesUpdate(blog.id)} />
       )}
     </div>
