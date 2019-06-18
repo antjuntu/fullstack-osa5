07beefa9ab2f341ebf13475c10b0b6a53c0e37a7
antjuntu <antjuntu@gmail.com>
2019-06-18 17:07:19 +0300

    5.14: blogilistan testit, step2

--- bloglist-frontend/src/components/SimpleBlog.test.js
+++ bloglist-frontend/src/components/SimpleBlog.test.js
@@ -27,3 +27,23 @@ test('renders the blogs title, author and likes', () => {
     'blog has 7 likes'
   )
 })
+
+test('clicking the like button twice calls event handler twice', () => {
+  const blog = {
+    title: 'Testiblogin otsikko',
+    author: 'Teppo Testaaja',
+    likes: 7
+  }
+
+  const mockHandler = jest.fn()
+
+  const component = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  )
+
+  const button = component.getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
+})
